7d8a648bf1b0a19e290f5f64eb564a43b2120790
Furkan Altıntaş <furkanaltintas785@gmail.com>
2022-05-16 22:51:48 +0300

    app module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import { ToastrModule } from 'ngx-toastr';
+import { NgxSpinnerModule } from "ngx-spinner";
+
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { AdminLayoutComponent } from './components/admin/admin-layout/admin-layout.component';
-import { AdminFooterComponent } from './components/admin/admin-layout/admin-footer/admin-footer.component';
-import { AdminNavbarComponent } from './components/admin/admin-layout/admin-navbar/admin-navbar.component';
 import { AdminSidenavComponent } from './components/admin/admin-layout/admin-sidenav/admin-sidenav.component';
+import { AdminFooterComponent } from './components/admin/admin-layout/admin-footer/admin-footer.component';
 import { AdminRightSidenavComponent } from './components/admin/admin-layout/admin-right-sidenav/admin-right-sidenav.component';
+import { AdminNavbarComponent } from './components/admin/admin-layout/admin-navbar/admin-navbar.component';
 import { AdminTeachersComponent } from './components/admin/admin-teachers/admin-teachers.component';
-import { AdminStudentsComponent } from './components/admin/admin-students/admin-students.component';
 import { AdminLessonsComponent } from './components/admin/admin-lessons/admin-lessons.component';
+import { AdminClassesComponent } from './components/admin/admin-classes/admin-classes.component';
 import { TeachersComponent } from './components/teachers/teachers.component';
 import { TeacherLayoutComponent } from './components/teachers/teacher-layout/teacher-layout.component';
 import { TeacherSidenavComponent } from './components/teachers/teacher-layout/teacher-sidenav/teacher-sidenav.component';
-import { TeacherFooterComponent } from './components/teachers/teacher-layout/teacher-footer/teacher-footer.component';
 import { TeacherNavbarComponent } from './components/teachers/teacher-layout/teacher-navbar/teacher-navbar.component';
+import { TeacherFooterComponent } from './components/teachers/teacher-layout/teacher-footer/teacher-footer.component';
 import { TeacherRightSidenavComponent } from './components/teachers/teacher-layout/teacher-right-sidenav/teacher-right-sidenav.component';
-import { StudentsLayoutComponent } from './components/students/students-layout/students-layout.component';
-import { StudentsFooterComponent } from './components/students/students-layout/students-footer/students-footer.component';
-import { StudentsNavbarComponent } from './components/students/students-layout/students-navbar/students-navbar.component';
-import { StudentsRightSidenavComponent } from './components/students/students-layout/students-right-sidenav/students-right-sidenav.component';
-import { StudentsSidenavComponent } from './components/students/students-layout/students-sidenav/students-sidenav.component';
-import { TeachersTestsComponent } from './components/teachers/teachers-tests/teachers-tests.component';
 import { TeachersAnnouncementsComponent } from './components/teachers/teachers-announcements/teachers-announcements.component';
+import { TeachersTestsComponent } from './components/teachers/teachers-tests/teachers-tests.component';
 import { TeachersLecturenotesComponent } from './components/teachers/teachers-lecturenotes/teachers-lecturenotes.component';
-import { ToastrModule } from 'ngx-toastr';
+import { StudentsComponent } from './components/students/students.component';
+import { StudentsLayoutComponent } from './components/students/students-layout/students-layout.component';
+import { StudentsNavbarComponent } from './components/students/students-layout/students-navbar/students-navbar.component';
+import { StudentsSidenavComponent } from './components/students/students-layout/students-sidenav/students-sidenav.component';
+import { StudentsFooterComponent } from './components/students/students-layout/students-footer/students-footer.component';
+import { StudentsRightSidenavComponent } from './components/students/students-layout/students-right-sidenav/students-right-sidenav.component';
+import { StudentsTestsComponent } from './components/students/students-tests/students-tests.component';
 import { StudentsLecturenotesComponent } from './components/students/students-lecturenotes/students-lecturenotes.component';
 import { StudentsReportCardComponent } from './components/students/students-reportcard/students-reportcard.component';
-import { StudentsTestsComponent } from './components/students/students-tests/students-tests.component';
-import { StudentsLoginComponent } from './components/students/students-login/students-login.component';
+import { AdminTeacherAddComponent } from './components/admin/admin-teachers/admin-teacher-add/admin-teacher-add.component';
+import { TeacherPipe } from './components/admin/admin-teachers/pipes/teacher.pipe';
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -39,35 +50,42 @@ import { StudentsLoginComponent } from './components/students/students-login/stu
     HomeComponent,
     AdminComponent,
     AdminLayoutComponent,
-    AdminFooterComponent,
-    AdminNavbarComponent,
     AdminSidenavComponent,
+    AdminFooterComponent,
     AdminRightSidenavComponent,
+    AdminNavbarComponent,
     AdminTeachersComponent,
-    AdminStudentsComponent,
+    AdminSidenavComponent,
     AdminLessonsComponent,
+    AdminClassesComponent,
     TeachersComponent,
     TeacherLayoutComponent,
     TeacherSidenavComponent,
-    TeacherFooterComponent,
     TeacherNavbarComponent,
+    TeacherFooterComponent,
     TeacherRightSidenavComponent,
-    StudentsLayoutComponent,
-    StudentsFooterComponent,
-    StudentsNavbarComponent,
-    StudentsRightSidenavComponent,
-    StudentsSidenavComponent,
-    TeachersTestsComponent,
     TeachersAnnouncementsComponent,
+    TeachersTestsComponent,
     TeachersLecturenotesComponent,
+    StudentsComponent,
+    StudentsLayoutComponent,
+    StudentsNavbarComponent,
+    StudentsSidenavComponent,
+    StudentsFooterComponent,
+    StudentsRightSidenavComponent,
+    StudentsTestsComponent,
     StudentsLecturenotesComponent,
     StudentsReportCardComponent,
-    StudentsTestsComponent,
-    StudentsLoginComponent
+    AdminTeacherAddComponent,
+    TeacherPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    NgxSpinnerModule,
+    SweetAlert2Module.forRoot(),
     ToastrModule.forRoot({
       closeButton: true,
       progressBar: true
